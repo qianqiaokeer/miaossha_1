c975a950e83219b84e14ba3e0de9e8bf33dcd715
zhangkehui <1563239534@qq.com>
2021-12-05 09:10:37 +0800

    修改

--- src/main/java/com/imooc/miaosha/MyApplication.java
+++ src/main/java/com/imooc/miaosha/MyApplication.java
@@ -2,7 +2,11 @@ package com.imooc.miaosha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 启动程序
+ * @author ke
+ *
+ */
 @SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
