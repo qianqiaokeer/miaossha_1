d2e300b181c476173392bcc32affad8b63edfa5f
zhangkehui <1563239534@qq.com>
2021-12-22 21:44:45 +0800

    修改

--- src/main/java/com/imooc/miaosha/MyApplication.java
+++ src/main/java/com/imooc/miaosha/MyApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
